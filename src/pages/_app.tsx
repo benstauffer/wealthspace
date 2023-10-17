9a17db1f1157549a809e83043a8f245dc1b01088
Ben Stauffer <benstauffer@Bens-Air.lan>
2023-10-17 01:52:05 -0700

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,30 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider, SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <SignedIn>
-          <UserButton />
-          <Component {...pageProps} />
-      </SignedIn>
-      <SignedOut>
-          <SignIn />
-      </SignedOut>
-
-    </ClerkProvider>
+    <main className="flex justify-center mt-5">
+      <div className="h-full w-full md:max-w-5xl">
+        <ClerkProvider {...pageProps}>
+          <SignedIn>
+            <UserButton />
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <SignIn />
+          </SignedOut>
+        </ClerkProvider>
+      </div>
+    </main>
   );
-}
+};
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,31 @@ import Link from "next/link";
 
 import { api } from "~/utils/api";
 import { UserButton } from "@clerk/nextjs";
+import { DialogAddAccount } from "~/@/components/ui/add-account";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/@/components/ui/dialog";
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
   const { data } = api.accounts.getAll.useQuery();
 
   return (
     <>
-      <main className="flex justify-center">
-        <div className="bg-red-200">
-          <title>Create T3 App</title>
+      <main className="mt-5 flex justify-center">
+        <div className="h-full w-full md:max-w-5xl">
+          <div>
+            <DialogAddAccount />
+          </div>
+          <div>
+            <Button>Click Here</Button>          
+          </div>
           <div>
             {data?.map((account) => <div key={account.id}>{account.name}</div>)}
           </div>
