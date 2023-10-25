6dc2a3f30d0ac8a8ed12b0bd67dc37e3cd17cb9a
Ben Stauffer <benstauffer@Bens-Air.lan>
2023-10-24 19:15:29 -0700

    Table Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,32 +5,23 @@ import { api } from "~/utils/api";
 import { UserButton } from "@clerk/nextjs";
 import { DialogAddAccount } from "~/@/components/ui/add-account";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/@/components/ui/dialog";
-import { Button } from "@radix-ui/themes";
+import DataTable from "~/@/components/ui/accounts-table/data-table";
 
 export default function Home() {
   const { data } = api.accounts.getAll.useQuery();
-
   return (
     <>
       <main className="mt-5 flex justify-center">
         <div className="h-full w-full md:max-w-3xl">
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <DialogAddAccount/>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <DialogAddAccount />
           </div>
           <div>
-            {data?.map((account) => <div key={account.id}>{account.name}</div>)}
+            {/* {data?.map((account) => <div key={account.id}>{account.type}</div>)} */}
+            <DataTable data={data || []} />
           </div>
         </div>
       </main>
     </>
   );
 }
-
